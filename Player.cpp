fe5bcfdc3fb6e7acb4addd222142fcc3dfbb9da3
Osmancan Çağlayan <134306439+Caglyn@users.noreply.github.com>
2023-05-23 16:24:15 +0300

    Add files via upload

--- Player.cpp
+++ Player.cpp
@@ -1,8 +1,8 @@
 #include "player.h"
 
-Player::Player(QGraphicsItem *parent) : QGraphicsPixmapItem(parent)
+Player::Player(const QString& imagePath, QGraphicsItem *parent) : QGraphicsPixmapItem(parent)
 {
-    QPixmap playerImage(":/Images/kenney_platformer-art-extended-enemies/Alien sprites/alienGreen.png"); // Replace ":/images/player.png" with the actual path to your image file
+    QPixmap playerImage(imagePath);
     setPixmap(playerImage.scaled(50, 50)); // Set the size of the player image
     stepSize = 10;
 }
@@ -11,28 +11,36 @@ void Player::keyPressEvent(QKeyEvent *event)
 {
     switch (event->key()) {
     case Qt::Key_Left:
-        moveBy(-stepSize, 0);
+        if(pos().x() > 0)
+            moveBy(-stepSize, 0);
         break;
     case Qt::Key_Right:
-        moveBy(stepSize, 0);
+        if(pos().x() + 40 < 800)
+            moveBy(stepSize, 0);
         break;
     case Qt::Key_Up:
-        moveBy(0, -stepSize);
+        if(pos().y() > 0)
+            moveBy(0, -stepSize);
         break;
     case Qt::Key_Down:
-        moveBy(0, stepSize);
+        if(pos().y() + 50 < 600)
+            moveBy(0, stepSize);
         break;
     case Qt::Key_A:
-        moveBy(-stepSize, 0);
+        if(pos().x() > 0)
+            moveBy(-stepSize, 0);
         break;
     case Qt::Key_D:
-        moveBy(stepSize, 0);
+        if(pos().x() + 40 < 800)
+            moveBy(stepSize, 0);
         break;
     case Qt::Key_W:
-        moveBy(0, -stepSize);
+        if(pos().y() > 0)
+            moveBy(0, -stepSize);
         break;
     case Qt::Key_S:
-        moveBy(0, stepSize);
+        if(pos().y() + 50 < 600)
+            moveBy(0, stepSize);
         break;
     default:
         break;
--- main.cpp
+++ main.cpp
@@ -6,8 +6,10 @@ int main(int argc, char *argv[])
     QApplication app(argc, argv);
 
     GameView view;
-    view.setSceneRect(0, 0, 400, 400);
-    view.setFixedSize(400, 400);
+    view.setSceneRect(0, 0, 800, 600);
+    view.setFixedSize(800, 600);
+    view.setHorizontalScrollBarPolicy(Qt::ScrollBarAlwaysOff);
+    view.setVerticalScrollBarPolicy(Qt::ScrollBarAlwaysOff);
     view.show();
 
     return app.exec();
