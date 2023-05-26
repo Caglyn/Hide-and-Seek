23d3dbc44d307a5b8008a1f12737afba06f7b6f7
sirem21 <134364808+sirem21@users.noreply.github.com>
2023-05-26 13:22:23 +0300

    gameview1.cpp
    updated

--- gameview1.cpp
+++ gameview1.cpp
@@ -1,15 +1,24 @@
-#include "gameview1.h"
+#include "gameview.h"
+#include "qapplication.h"
+#include <QInputDialog>
+#include <QMessageBox>
+#include <QString>
 
+//constructor
 GameView::GameView(QWidget *parent) : QGraphicsView(parent), scene(new QGraphicsScene(this)), scorePlayer1(0), scorePlayer2(0)
 {
     setScene(scene);
-    scene->setBackgroundBrush(Qt::black);
+    //scene->setBackgroundBrush(Qt::black);
+    scene->setBackgroundBrush(QBrush(QImage(":/Images/kenney_platformer-art-extended-enemies/Sky/sky.jpg")));
 
     player1 = new Player(":/Images/kenney_platformer-art-extended-enemies/Alien sprites/alienPink.png"); // Replace ":/images/player1.png" with the actual path to player1's image file
     player2 = new Player(":/Images/kenney_platformer-art-extended-enemies/Alien sprites/alienGreen.png"); // Replace ":/images/player2.png" with the actual path to player2's image file
-    player2->setPos(200, 200); // Set initial position for player2
+    player1->setPos(200, 50);// Set initial position for player1
+    player2->setPos(100, 50);// Set initial position for player2
+
     //player1->setZValue(1);
     //player2->setZValue(1);
+    setFrameShape(QFrame::NoFrame);
 
     scene->addItem(player1);
     scene->addItem(player2);
@@ -23,11 +32,13 @@ GameView::GameView(QWidget *parent) : QGraphicsView(parent), scene(new QGraphics
     setRenderHint(QPainter::Antialiasing);
     setWindowTitle("Keyboard Input Game");
 
-    QTimer* collisionTimer = new QTimer(this);
-    connect(collisionTimer, &QTimer::timeout, this, &GameView::checkCollisions);
-    collisionTimer->start(100); // Adjust the interval as needed
-}
 
+        QTimer* collisionTimer = new QTimer(this);
+        connect(collisionTimer, &QTimer::timeout, this, &GameView::checkCollisions);
+        collisionTimer->start(100); // Adjust the interval as needed
+
+}
+//destructor
 GameView::~GameView()
 {
     qDeleteAll(ghosts);
@@ -46,7 +57,8 @@ void GameView::keyPressEvent(QKeyEvent *event)
     }
 }
 
-void GameView::checkCollisions()
+//using the inline function
+inline void GameView::checkCollisions()
 {
     foreach (Ghost* ghost, ghosts) {
         if (ghost->collidesWithItem(player1)) {
@@ -81,6 +93,7 @@ void GameView::checkCollisions()
         QMessageBox::information(this, "Game Over", message);
         QApplication::quit();
     }
+
 }
 
 void GameView::addGhost(const QString& imagePath, qreal x, qreal y)
@@ -92,13 +105,21 @@ void GameView::addGhost(const QString& imagePath, qreal x, qreal y)
     ghost->startMoving();
 }
 
+//using of exception handling
 void GameView::updateScore(Player* player)
 {
-    if (player == player1) {
-        scorePlayer1++;
-        qDebug() << "Player 1 Score: " << scorePlayer1;
-    } else if (player == player2) {
-        scorePlayer2++;
-        qDebug() << "Player 2 Score: " << scorePlayer2;
+    try {
+        if (player == player1) {
+            scorePlayer1++;
+            qDebug() << "Player 1 Score: " << scorePlayer1;
+        } else if (player == player2) {
+            scorePlayer2++;
+            qDebug() << "Player 2 Score: " << scorePlayer2;
+        } else {
+            throw std::invalid_argument("Invalid player provided.");
+        }
+    } catch (const std::exception& e) {
+        qDebug() << "Error updating score: " << e.what();
     }
 }
+
